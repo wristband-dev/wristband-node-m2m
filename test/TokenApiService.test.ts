131974d1239e1691b2faf369ddf467db18bd0fa9
bryanhsiao <bryhsiao@gmail.com>
2023-11-26 13:31:59 -0800

    have unit test changed with mock lib nock

--- test/TokenApiService.test.ts
+++ test/TokenApiService.test.ts
@@ -1,16 +1,49 @@
-import { WristbandM2MSdk } from '../src/index'
-//import { WristbandM2MSdk } from 'wristband-node-m2m'
-test('getToken', async() => {
+import nock from 'nock';
 
-  const APPLICATION_DOMAIN='appDomain';
-  const CLIENT_ID='clientId';
-  const CLIENT_SECRET='clientSecret';
-  const sdk = new WristbandM2MSdk( {
-    appDomain: APPLICATION_DOMAIN, clientId: CLIENT_ID, clientSecret: CLIENT_SECRET
+import { WristbandM2MSdk } from '../src/index'
+
+
+describe('test TokenApIs', () => {
+  let sdk: any;
+
+  beforeEach(() => {
+    const APPLICATION_DOMAIN = 'localhost:3001';
+    const CLIENT_ID = 'clientId';
+    const CLIENT_SECRET = 'clientSecret';
+    sdk = new WristbandM2MSdk({
+      appDomain: APPLICATION_DOMAIN, clientId: CLIENT_ID, clientSecret: CLIENT_SECRET
+    });
+
+    nock.cleanAll();
   });
 
-  const result = await sdk.getToken();
-  const cachedResult = await sdk.getToken();
-  expect(result).toEqual(cachedResult);
+  describe('test tokenV1', () => {
+    test('test api call', () => {
+      const scope = nock('https://localhost:3001')
+        .post('/api/v1/oauth2/token')
+        .reply(200, "");
+      return sdk.getToken().then((r: any) => expect(r).toEqual(undefined));
+    });
+  });
+
+
 })
+
+
+
+// import { WristbandM2MSdk } from '../src/index'
+// //import { WristbandM2MSdk } from 'wristband-node-m2m'
+// test('getToken', async() => {
+//
+//   const APPLICATION_DOMAIN='appDomain';
+//   const CLIENT_ID='clientId';
+//   const CLIENT_SECRET='clientSecret';
+//   const sdk = new WristbandM2MSdk( {
+//     appDomain: APPLICATION_DOMAIN, clientId: CLIENT_ID, clientSecret: CLIENT_SECRET
+//   });
+//
+//   const result = await sdk.getToken();
+//   const cachedResult = await sdk.getToken();
+//   expect(result).toEqual(cachedResult);
+// })
 

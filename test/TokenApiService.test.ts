c1b706330fe5d2f6f8d291bc720ea72c50555dd5
bryanhsiao <bryhsiao@gmail.com>
2023-11-26 23:02:40 -0800

    refactorred package.json and disabled webpack & filetransformer.

--- test/TokenApiService.test.ts
+++ test/TokenApiService.test.ts
@@ -2,7 +2,6 @@ import nock from 'nock';
 
 import { WristbandM2MSdk } from '../src/index'
 
-
 describe('test TokenApIs', () => {
   let sdk: any;
 
@@ -29,21 +28,4 @@ describe('test TokenApIs', () => {
 
 })
 
-
-
-// import { WristbandM2MSdk } from '../src/index'
-// //import { WristbandM2MSdk } from 'wristband-node-m2m'
-// test('getToken', async() => {
-//
-//   const APPLICATION_DOMAIN='appDomain';
-//   const CLIENT_ID='clientId';
-//   const CLIENT_SECRET='clientSecret';
-//   const sdk = new WristbandM2MSdk( {
-//     appDomain: APPLICATION_DOMAIN, clientId: CLIENT_ID, clientSecret: CLIENT_SECRET
-//   });
-//
-//   const result = await sdk.getToken();
-//   const cachedResult = await sdk.getToken();
-//   expect(result).toEqual(cachedResult);
-// })
 

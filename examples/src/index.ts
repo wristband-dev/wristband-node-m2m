a43383823ea390491514497af86984cbbdf0372a
bryanhsiao <bryhsiao@gmail.com>
2023-12-01 02:28:18 -0800

    updated the examples and test

--- examples/src/index.ts
+++ examples/src/index.ts
@@ -3,6 +3,9 @@ import {WristbandM2MClient} from 'wristband-node-m2m';
 const APPLICATION_DOMAIN = 'appDomain';
 const CLIENT_ID = 'clientId';
 const CLIENT_SECRET = 'clientSecret';
+// const APPLICATION_DOMAIN='invotasticb2bm2m-bryantesttenant.us.wristband.dev';
+// const CLIENT_ID='dqroj4gedba73pymwdv5wdbjtm';
+// const CLIENT_SECRET='1ff5068914201d80d000005eeda43ec4';
 
 const wristbandM2MClient = new WristbandM2MClient({
   appDomain: APPLICATION_DOMAIN, clientId: CLIENT_ID, clientSecret: CLIENT_SECRET
--- test/TokenApiService.test.ts
+++ test/TokenApiService.test.ts
@@ -2,6 +2,7 @@ import nock from 'nock';
 
 import { WristbandM2MClient } from '../src/index'
 
+
 describe('test TokenApis', () => {
   let wristbandM2MClient: any;
 
@@ -12,18 +13,19 @@ describe('test TokenApis', () => {
     wristbandM2MClient = new WristbandM2MClient({
       appDomain: APPLICATION_DOMAIN, clientId: CLIENT_ID, clientSecret: CLIENT_SECRET
     });
-
     nock.cleanAll();
   });
 
   describe('test tokenV1', () => {
-    test('test api call', () => {
-      const scope = nock('https://localhost:3001')
-        .post('/api/v1/oauth2/token')
-        .reply(200, "");
-      return wristbandM2MClient.getToken().then((r: any) => expect(r).toEqual(undefined));
+    test('test api call', async () => {
+
+      const scope = nock('https://localhost:3001/api')
+        .persist()
+        .post('/v1/oauth2/token', 'grant_type=client_credentials')
+        .reply(200, {data: {access_token: 'accessToken', expires_in: 'expiresIn'}});
+
+      return await wristbandM2MClient.getToken().then((r: any) => expect(r).toEqual(undefined));
     });
   });
 })
-
 
